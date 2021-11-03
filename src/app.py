b09d98f00dbe641b8d4cbee619ce883516b3152e
Jakub Hanes <jakvbko@gmail.com>
2021-11-03 18:46:10 +0100

    invoice method

--- src/app.py
+++ src/app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+import logging
 import lnd_grpc
+from flask import request, Response, Flask
 from decouple import config
 # import lnurl
 
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+log.addHandler(logging.StreamHandler())
+
+app = Flask('app')
 lnd_rpc = lnd_grpc.Lightning(
             lnd_dir=config('lnd_dir'),
             macaroon_path=config('macaroon_path'),
@@ -10,13 +16,14 @@ lnd_rpc = lnd_grpc.Lightning(
             network='mainnet',
             grpc_host='localhost',
             grpc_port='10009')
-res = lnd_rpc.add_invoice(value=100)
-print(res)
-print(type(res))
-print(dict(res))
-app = Flask('app')
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/invoice/add", methods=['POST'])
+def add():
+    request_data = request.get_json()
+    value = request_data['value']
+    value = max(min(int(value), 5000), 100)
+    res = lnd_rpc.add_invoice(value=value)
+    print(res)
+    return res.payment_request
+
